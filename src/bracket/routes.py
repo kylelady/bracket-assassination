669c8a32799b155eae0e897407fe20d760b07272
David Adrian <davidcadrian@gmail.com>
2014-03-04 20:22:23 -0500

    Solve it with an iframe

--- src/bracket/routes.py
+++ src/bracket/routes.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/index')
 def index():
     return render_template('index.view')
+
+@app.route('/bracket')
+def bracket():
+	return render_template('bracket.view')
